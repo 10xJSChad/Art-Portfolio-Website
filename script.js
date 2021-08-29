54c3cd4674a0f612705d0ae1e51041b6a4c8df11
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-08-29 15:07:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+  
+
+function initialize(){
+    var main_container = document.getElementById("main-container");
+    var intro_container = document.getElementById("intro-container");
+    setTimeout(function() {
+        intro_container.parentNode.removeChild(intro_container);
+   }, 1); //6200 and 8.5s
+
+   const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+window.addEventListener('load', function () {
+    initialize();
+})
